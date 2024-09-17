995210e7acaaa81fa3f06e057554dd768111361d
zhangxinsen <zhangxinsen@ane56.com>
2024-09-17 10:21:07 +0800

    使用ApplicationUtils构建并且简化主流程

--- src/main/java/com/fishsun/bigdata/flink/Kafka2IcebergApp.java
+++ src/main/java/com/fishsun/bigdata/flink/Kafka2IcebergApp.java
@@ -1,28 +1,15 @@
 package com.fishsun.bigdata.flink;
 
-import com.fishsun.bigdata.utils.HiveSchemaUtils;
-import com.fishsun.bigdata.utils.KafkaUtils;
-import com.fishsun.bigdata.utils.ParamUtils;
+import com.fishsun.bigdata.utils.ApplicationUtils;
 import com.fishsun.bigdata.utils.StreamUtils;
-import org.apache.flink.api.common.eventtime.WatermarkStrategy;
-import org.apache.flink.connector.kafka.source.KafkaSource;
-import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.table.api.TableSchema;
-import org.apache.flink.table.api.constraints.UniqueConstraint;
-import org.apache.flink.types.Row;
-import org.apache.iceberg.flink.TableLoader;
-import org.apache.iceberg.flink.sink.FlinkSink;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 
 import static com.fishsun.bigdata.utils.IcebergUtils.HIVE_CATALOG_NS_NAME;
 import static com.fishsun.bigdata.utils.IcebergUtils.HIVE_CATALOG_TBL_NAME;
-import static com.fishsun.bigdata.utils.IcebergUtils.getTableLoader;
 import static com.fishsun.bigdata.utils.ParamUtils.enhanceConfig;
 import static com.fishsun.bigdata.utils.ParamUtils.parseConfig;
 
@@ -34,86 +21,23 @@ import static com.fishsun.bigdata.utils.ParamUtils.parseConfig;
  * @Desc :
  */
 public class Kafka2IcebergApp {
-  private static final Logger logger = LoggerFactory.getLogger(Kafka2IcebergApp.class);
 
   public static void main(String[] args) throws Exception {
     Map<String, String> paramMap = parseConfig(args);
     enhanceConfig(paramMap);
-    // 1. 创建 Flink 执行环境
+    // 创建 Flink 执行环境
     // local=false(默认) --> 集群环境
     // local=true(本地webUI) --> 本地环境 rest.port配置端口
     StreamExecutionEnvironment env =
             StreamUtils.getStreamEnv(paramMap);
 
-    // 2. 初始化HiveSchemaUtils
-    HiveSchemaUtils hiveSchemaUtils = HiveSchemaUtils.buildFromParamMap(
+    ApplicationUtils.setupPipelines(
+            env,
             paramMap
     );
 
-    // 3. 从 Kafka 读取数据
-    logger.info("kafka source initializing");
-    System.out.println("kafka source initializing");
-    KafkaSource<Row> kafkaSource =
-            KafkaUtils.getKafkaSource(paramMap);
-    logger.info("successfully get kafka source");
-    System.out.println("successfully get kafka source");
-    DataStream<Row> kafkaStream = env.fromSource(
-            kafkaSource,
-            WatermarkStrategy.noWatermarks(),
-            "Kafka Source"
-    );
-    logger.info("kafka source init finished");
-    System.out.println("kafka source init finished");
-    // 4. 定义 Iceberg 表的 TableSchema
-    TableSchema tableSchema = hiveSchemaUtils.toFlinkTableSchema(
-            paramMap.get(HIVE_CATALOG_NS_NAME),
-            paramMap.get(HIVE_CATALOG_TBL_NAME),
-            null,
-            ParamUtils.getPrimaryKeys(paramMap)
-    );
-    List<String> uniqueCols = new LinkedList<>();
-    if (tableSchema.getPrimaryKey().isPresent()) {
-      UniqueConstraint uniqueConstraint = tableSchema.getPrimaryKey().get();
-      uniqueCols = uniqueConstraint.getColumns();
-    }
-    logger.info("table schema has been initialized");
-    System.out.println("table schema has been initialized");
-    // 5. 加载 Iceberg 表
-    TableLoader tableLoader = getTableLoader(paramMap);
-    logger.info("iceberg table has been loader successfully");
-    System.out.println("iceberg table has been load successfully");
-    // 6. 将数据写入 Iceberg 表
-    if (uniqueCols.isEmpty()) {
-      logger.info("iceberg sink will be OK without primary keys using append stream");
-      System.out.println("iceberg sink will be OK without primary keys using append stream");
-      FlinkSink.forRow(kafkaStream, tableSchema)
-              .tableLoader(tableLoader)
-              .tableSchema(tableSchema)
-              .overwrite(false)
-              .upsert(false)  // 开启 UPSERT 模式
-              .append();
-    } else {
-      logger.info("iceberg sink will be OK with primary keys using upsert stream");
-      System.out.println("iceberg sink will be OK with primary keys using upsert stream");
-      logger.info("got {} primary keys", uniqueCols.size());
-      System.out.println("got " + uniqueCols.size() + " primary keys");
-      for (int i = 0; i < uniqueCols.size(); i++) {
-        logger.info("primary key {} is {}", i + 1, uniqueCols.get(i));
-        System.out.println("primary key " + (i + 1) + " is " + uniqueCols.get(i));
-      }
-      FlinkSink.forRow(kafkaStream, tableSchema)
-              .tableLoader(tableLoader)
-              .tableSchema(tableSchema)
-              .overwrite(false)
-              .upsert(true)  // 开启 UPSERT 模式
-              .equalityFieldColumns(uniqueCols)
-              .append();
-      logger.info("sink has been OK");
-      System.out.println("sink has been OK");
-    }
 
-
-    // 11. 启动 Flink 作业
+    // 启动 Flink 作业
     env.execute("Flink Kafka to Iceberg with Additional Fields");
   }
 
---
+++ src/main/java/com/fishsun/bigdata/utils/ApplicationUtils.java
@@ -0,0 +1,151 @@
+package com.fishsun.bigdata.utils;
+
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.TableSchema;
+import org.apache.flink.table.api.constraints.UniqueConstraint;
+import org.apache.flink.types.Row;
+import org.apache.iceberg.flink.TableLoader;
+import org.apache.iceberg.flink.sink.FlinkSink;
+import org.apache.thrift.TException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import static com.fishsun.bigdata.utils.IcebergUtils.HIVE_CATALOG_NS_NAME;
+import static com.fishsun.bigdata.utils.IcebergUtils.HIVE_CATALOG_TBL_NAME;
+import static com.fishsun.bigdata.utils.IcebergUtils.getTableLoader;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @Author : zhangxinsen
+ * @create : 2024/9/16 23:28
+ * @Desc :
+ */
+public class ApplicationUtils {
+  private static final Logger logger = LoggerFactory.getLogger(ApplicationUtils.class);
+
+  /**
+   * 构建kafkaSource
+   *
+   * @param env
+   * @param paramMap
+   * @return
+   * @throws TException
+   */
+  public static DataStream<Row> setupKafkaSource(StreamExecutionEnvironment env, Map<String, String> paramMap) throws TException {
+    String hiveTblName = String.format("%s-%s", paramMap.get(HIVE_CATALOG_NS_NAME), paramMap.get(HIVE_CATALOG_TBL_NAME));
+    // 从 Kafka 读取数据
+    logger.info("kafka source initializing for " + hiveTblName);
+    System.out.println("kafka source initializing for " + hiveTblName);
+    KafkaSource<Row> kafkaSource =
+            KafkaUtils.getKafkaSource(paramMap);
+    logger.info("successfully get kafka source for " + hiveTblName);
+    System.out.println("successfully get kafka source for " + hiveTblName);
+    DataStream<Row> kafkaStream = env.fromSource(
+                    kafkaSource,
+                    WatermarkStrategy.noWatermarks(),
+                    "Kafka Source"
+            ).uid(hiveTblName + "-kafka-source")
+            .name(hiveTblName + "-kafka-source");
+    logger.info("kafka source init finished for" + hiveTblName);
+    System.out.println("kafka source init finished for" + hiveTblName);
+    return kafkaStream;
+  }
+
+  public static TableSchema setupTableSchema(Map<String, String> paramMap) throws TException {
+    HiveSchemaUtils hiveSchemaUtils = HiveSchemaUtils.buildFromParamMap(
+            paramMap
+    );
+    String hiveTblName = String.format("%s-%s", paramMap.get(HIVE_CATALOG_NS_NAME), paramMap.get(HIVE_CATALOG_TBL_NAME));
+    TableSchema tableSchema = hiveSchemaUtils.toFlinkTableSchema(
+            paramMap.get(HIVE_CATALOG_NS_NAME),
+            paramMap.get(HIVE_CATALOG_TBL_NAME),
+            null,
+            ParamUtils.getPrimaryKeys(paramMap)
+    );
+    logger.info(hiveTblName + " table schema has been initialized");
+    System.out.println(hiveTblName + " table schema has been initialized");
+    hiveSchemaUtils.close();
+    return tableSchema;
+  }
+
+  public static TableLoader setupTableLoader(Map<String, String> paramMap) {
+    String hiveTblName = String.format("%s-%s", paramMap.get(HIVE_CATALOG_NS_NAME), paramMap.get(HIVE_CATALOG_TBL_NAME));
+    TableLoader tableLoader = getTableLoader(paramMap);
+    logger.info(hiveTblName + " iceberg table has been loader successfully");
+    System.out.println(hiveTblName + " iceberg table has been load successfully");
+    return tableLoader;
+  }
+
+  public static void setupSink(DataStream<Row> kafkaStream, TableSchema tableSchema, TableLoader tableLoader, Map<String, String> paramMap) {
+    String hiveTblName = String.format("%s-%s", paramMap.get(HIVE_CATALOG_NS_NAME), paramMap.get(HIVE_CATALOG_TBL_NAME));
+    List<String> uniqueCols = new LinkedList<>();
+    if (tableSchema.getPrimaryKey().isPresent()) {
+      UniqueConstraint uniqueConstraint = tableSchema.getPrimaryKey().get();
+      uniqueCols = uniqueConstraint.getColumns();
+    }
+
+    // 6. 将数据写入 Iceberg 表
+    if (uniqueCols.isEmpty()) {
+      logger.info("iceberg sink will be OK without primary keys using append stream");
+      System.out.println("iceberg sink will be OK without primary keys using append stream");
+      FlinkSink.forRow(kafkaStream, tableSchema)
+              .tableLoader(tableLoader)
+              .tableSchema(tableSchema)
+              .overwrite(false)
+              .upsert(false)  // 开启 UPSERT 模式
+              .append()
+              .uid(hiveTblName + "-iceberg-sink")
+              .name(hiveTblName + "-iceberg-sink");
+    } else {
+      logger.info("iceberg sink will be OK with primary keys using upsert stream");
+      System.out.println("iceberg sink will be OK with primary keys using upsert stream");
+      logger.info("got {} primary keys", uniqueCols.size());
+      System.out.println("got " + uniqueCols.size() + " primary keys");
+      for (int i = 0; i < uniqueCols.size(); i++) {
+        logger.info("primary key {} is {}", i + 1, uniqueCols.get(i));
+        System.out.println("primary key " + (i + 1) + " is " + uniqueCols.get(i));
+      }
+      FlinkSink.forRow(kafkaStream, tableSchema)
+              .tableLoader(tableLoader)
+              .tableSchema(tableSchema)
+              .overwrite(false)
+              .upsert(true)  // 开启 UPSERT 模式
+              .equalityFieldColumns(uniqueCols)
+              .append()
+              .uid(hiveTblName + "-iceberg-sink")
+              .name(hiveTblName + "-iceberg-sink");
+    }
+    logger.info("sink has been OK");
+    System.out.println("sink has been OK");
+  }
+
+  public static void setupPipelines(StreamExecutionEnvironment env, Map<String, String> paramMap) throws TException {
+    // 1. 从 Kafka 读取数据
+    DataStream<Row> kafkaStream = setupKafkaSource(
+            env,
+            paramMap
+    );
+    // 2. 加载 Iceberg 表
+    TableLoader tableLoader = setupTableLoader(
+            paramMap
+    );
+
+    // 3. 定义 Iceberg 表的 TableSchema
+    TableSchema tableSchema = setupTableSchema(paramMap);
+    // 4. 构建sink
+    setupSink(
+            kafkaStream,
+            tableSchema,
+            tableLoader,
+            paramMap
+    );
+  }
+}
